97a006d770675832e21e3833f6647e56ed4c51ea
LOKESH KUMAR <lokeshr.net@gmail.com>
2026-05-28 18:48:04 +0530

    fixed frontend tracking

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App()
+{
+  return(
+    <div style={{textAlign:"center"}}>
+      <h1>React App Running.</h1>
+      <h1>Frontend CI Triggered.</h1>
+    </div>
+  )
+}
+
+export default App;
